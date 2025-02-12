2fe7789960ccb44703e3607845d80bbfba2c5e02
yilkalderseh123 <yilkalderseh@gmail.com>
2025-02-12 17:49:21 +0300

    first commit

--- backend/index1.js
+++ backend/index1.js
@@ -114,7 +114,7 @@ io.on("connection", (socket) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
