007beb54c1293fbc8f3904a837d0f29e28599b60
yilkalderseh123 <yilkalderseh@gmail.com>
2025-02-12 17:45:16 +0300

    backend port changed to 5001

--- backend/index1.js
+++ backend/index1.js
@@ -114,7 +114,7 @@ io.on("connection", (socket) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
