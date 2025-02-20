fa0f57510a93577e01a0935529ae99336e86e70c
yilkalderseh123 <yilkalderseh@gmail.com>
2025-02-20 09:31:52 +0300

    testimony post page added

--- backend/constant/url.js
+++ backend/constant/url.js
@@ -1,5 +1,5 @@
 // url.js
-const frontendURL = "http://localhost:4200";
+// const frontendURL = "http://localhost:4200";
 // const frontendURL = "https://savvybridge-ember.vercel.app";
 // const frontendURL = "http://49.12.205.35";
 module.exports = frontendURL;
