2ec044d08c74115e0309325e2ffaf76f8aae6573
yilkalderseh123 <yilkalderseh@gmail.com>
2025-02-26 21:47:36 +0300

    SEO added

--- frontend/app/routes/home.ts
+++ frontend/app/routes/home.ts
@@ -1,3 +1,21 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
-export default class HomeRoute extends Route {}
+export default class HomeRoute extends Route {
+  @service headData: any;
+
+  beforeModel() {
+    this.setSEO();
+  }
+
+  @action
+  setSEO() {
+    this.headData.title = 'Savvy Bridge - Your Partner in Strategy, Design, and Development';
+    this.headData.description = 'Savvy Bridge specializes in bringing digital products to success through expert strategy, design, product management, and development.';
+    this.headData.keywords = 'strategy, design, product management, development, digital products, tech consulting';
+    this.headData.image = '/images/blog1.jpg';
+  }
+}
